d90e3b5a3c970be4c001e861c3e48fc2302c5f8a
Lerototi <lerototi@gmail.com>
2019-09-06 08:25:22 -0300

    initial commit

---
+++ backend/knexfile.js
@@ -0,0 +1,19 @@
+// Update with your config settings.
+
+module.exports = {
+    client: 'postgresql',
+    connection: {
+      database: 'tasks',
+      user:     'postgres',
+      password: '1234'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  
+
+};
