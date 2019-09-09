44ee3d00d518b782db4728a6f5efa0e4ce76b397
Leonardo Torres <lerototi@gmail.com>
2019-09-09 14:06:29 -0300

    tasks CRUD

--- backend/api/task.js
+++ backend/api/task.js
@@ -20,5 +20,47 @@ module.exports = app => {
         req.body.userId = req.user.id
         app.db('tasks')
             .insert(req.body)
+            .then(_ => res.status(204).send())
     }
-}
+
+    const remove = (req, res) => {
+        app.db('tasks')
+            .where({ id: req.params.id, userId: req.user.id })
+            .del()
+            .then(rowsDeleted => {
+                if(rowsDeleted > 0){
+                    res.status(204).send()
+                }else {
+                    const msg = `Não foi encontrada taks com id ${req.params.id}.`
+                    res.status(400).send(msg)
+                }
+            })
+            .catch(err => res.status(400).json(err))
+    }
+
+    const updateTaskDoneAt = (req, res, doneAt) => {
+        app.db('tasks')
+            .where({ id: req.params.id, userId: req.user.id })
+            .update({ doneAt })
+            .then(_ => res.status(204).send())
+            .catch(err => res.status(400).json(err))
+    }
+
+    const toggleTask = (req, res) => {
+        app.db('tasks')
+            .where({ id: req.params.id, userId: req.user.id })
+            .first()
+            .then(task => {
+                if(!task) {
+                    const msg = `Task com id ${req.params.id} não encontrada.`
+                    return res.status(400).send(msg)
+                }
+
+                const doneAt = task.doneAt ? null : new Date()
+                updateTaskDoneAt(req, res, doneAt)
+            })
+            .catch(err => res.status(400).json(err))
+    }
+
+    return { getTasks, save, remove, toggleTask }
+ }
